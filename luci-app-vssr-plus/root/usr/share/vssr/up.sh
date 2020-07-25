e6e19857bb0273ef791f0fe3c97962f4460014ba
leishao1993 <416535868@qq.com>
2020-07-25 19:32:50 +0800

    vssr

--- luci-app-vssr-plus/root/usr/share/vssr/up.sh
+++ luci-app-vssr-plus/root/usr/share/vssr/up.sh
@@ -7,7 +7,7 @@
 
 wget -4 -O /usr/share/koolproxy/kpupdate https://raw.githubusercontent.com/firkerword/KPR/main/kpupdate  && chmod 775 /usr/share/koolproxy/kpupdate
 
-wget -4 -O /usr/bin/AdGuardHome/AdGuardHome https://raw.githubusercontent.com/firker/diy-ziyong/main/luci-app-adguardhome/root/usr/bin/AdGuardHome/AdGuardHome  && chmod 775 /usr/bin/AdGuardHome/AdGuardHome
+# wget -4 -O /usr/bin/AdGuardHome/AdGuardHome https://raw.githubusercontent.com/firker/diy-ziyong/main/luci-app-adguardhome/root/usr/bin/AdGuardHome/AdGuardHome  && chmod 775 /usr/bin/AdGuardHome/AdGuardHome
 
 # wget -4 -O /usr/share/netdata/web/dashboard.js  https://raw.githubusercontent.com/fw869/up/master/dashboard.js && chmod 664 /usr/share/netdata/web/dashboard.js
 
