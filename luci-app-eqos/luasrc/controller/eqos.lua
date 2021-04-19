db3d3d7c42a9c6e7625e5dfcb0d4a9dfaa2607c8
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-04-19 09:35:02 +0000

    luci-app: tidy up controller

--- luci-app-eqos/luasrc/controller/eqos.lua
+++ luci-app-eqos/luasrc/controller/eqos.lua
@@ -4,6 +4,7 @@ function index()
 	if not nixio.fs.access("/etc/config/eqos") then
 		return
 	end
+	
 	entry({"admin", "control"}, firstchild(), "Control", 50).dependent = false
 	entry({"admin", "control", "eqos"}, cbi("eqos"), _("EQOS"), 15).dependent = true
 end
