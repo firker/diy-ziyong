c53c0df1bf1500182d250a5f57f829d802e6ae46
firkerword <136000981@qq.com>
2021-05-20 22:27:52 +0800

    op

--- luci-app-adguardhome/root/usr/share/AdGuardHome/update_core.sh
+++ luci-app-adguardhome/root/usr/share/AdGuardHome/update_core.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 binpath=$(uci get AdGuardHome.AdGuardHome.binpath)
 if [ -z "$binpath" ]; then
@@ -130,21 +131,21 @@ doupdate_core(){
 	Arch="amd64"
 	;;
 	"mipsel")
-	Arch="mipsle"
-	;;
+		Arch="mipsle"
+		;;
 	"mips64el")
-	Arch="mips64le"
-	Arch="mipsle"
-	echo -e "mips64el use $Arch may have bug" 
-	;;
+		Arch="mips64le"
+		Arch="mipsle"
+		echo -e "mips64el use $Arch may have bug"
+		;;
 	"mips")
-	Arch="mips"
-	;;
+		Arch="mips"
+		;;
 	"mips64")
-	Arch="mips64"
-	Arch="mips"
-	echo -e "mips64 use $Arch may have bug" 
-	;;
+		Arch="mips64"
+		Arch="mips"
+		echo -e "mips64 use $Arch may have bug"
+		;;
 	"arm")
 	Arch="arm"
 	;;
