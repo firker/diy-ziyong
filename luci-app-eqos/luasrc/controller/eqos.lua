5b3f27bf60d9b735b40c4a43e0c8096b7334849c
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-11-18 07:12:53 +0000

    luci-app-eqos: tidy up code

--- luci-app-eqos/luasrc/controller/eqos.lua
+++ luci-app-eqos/luasrc/controller/eqos.lua
@@ -7,4 +7,12 @@ function index()
 	
 	entry({"admin", "control"}, firstchild(), "Control", 50).dependent = false
 	entry({"admin", "control", "eqos"}, cbi("eqos"), _("EQOS"), 15).dependent = true
+	entry({"admin", "control", "eqos", "status"}, call("act_status")).leaf = true
+end
+
+function act_status()
+	local e = {}
+	e.running = luci.sys.call("ip link | grep ifb >/dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
