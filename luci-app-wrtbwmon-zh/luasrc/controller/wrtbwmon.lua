361544a8d544b8d2b1320d6b150b697c7c6ffab2
firkerword <61251923+firkerword@users.noreply.github.com>
2022-12-11 11:01:41 +0800

    Update wrtbwmon.lua

--- luci-app-wrtbwmon-zh/luasrc/controller/wrtbwmon.lua
+++ luci-app-wrtbwmon-zh/luasrc/controller/wrtbwmon.lua
@@ -8,11 +8,11 @@ function index()
 	entry({"admin", "status", "usage"},alias("admin", "status", "usage", "details"), _("Traffic Status"), 60)
 	entry({"admin", "status", "usage", "details"}, template("wrtbwmon"),_("Details"), 10).leaf=true
 	entry({"admin", "status", "usage", "config"}, arcombine(cbi("wrtbwmon/config")),_("Configuration"), 20).leaf=true
-	entry({"admin", "status", "usage", "alias"}, form("wrtbwmon/alias"),_("Alias"), 30).leaf=true
+	entry({"admin", "status", "usage", "custom"}, form("wrtbwmon/custom"),_("User file"), 30).leaf=true
 	else
 	entry({"admin", "status", "usage"},alias("admin", "status", "usage", "details"), _("Traffic Status"), 60)
 	entry({"admin", "status", "usage", "details"}, template("wrtbwmon_"),_("Details"), 10).leaf=true
 	entry({"admin", "status", "usage", "config"}, arcombine(cbi("wrtbwmon/config")),_("Configuration"), 20).leaf=true
-	entry({"admin", "status", "usage", "alias"}, form("wrtbwmon/alias"),_("Alias"), 30).leaf=true
+	entry({"admin", "status", "usage", "custom"}, form("wrtbwmon/custom"),_("User file"), 30).leaf=true
 	end
 end
