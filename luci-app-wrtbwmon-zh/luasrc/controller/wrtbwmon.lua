2d09677840e121e5956f54130dd9f2141b9e9958
firkerword <61251923+firkerword@users.noreply.github.com>
2022-12-14 01:05:35 +0800

    Update wrtbwmon.lua

--- luci-app-wrtbwmon-zh/luasrc/controller/wrtbwmon.lua
+++ luci-app-wrtbwmon-zh/luasrc/controller/wrtbwmon.lua
@@ -4,9 +4,9 @@ function index()
 	if not nixio.fs.access("/etc/config/wrtbwmon") then 
                 return
         end
-	entry({"admin", "status", "usage"},alias("admin", "status", "usage", "details"), _("Traffic Status"), 60)
-	entry({"admin", "status", "usage", "details"}, template("wrtbwmon"),_("Details"), 10).leaf=true
-	entry({"admin", "status", "usage", "config"}, arcombine(cbi("wrtbwmon/config")),_("Configuration"), 20).leaf=true
-	entry({"admin", "status", "usage", "custom"}, form("wrtbwmon/custom"),_("User file"), 30).leaf=true
+	entry({"admin", "status", "usage"},alias("admin", "status", "usage", "details"),_("Traffic Status"), 60)
+	entry({"admin", "status", "usage", "details"},template("wrtbwmon"),_("Details"), 10).leaf=true
+	entry({"admin", "status", "usage", "config"},arcombine(cbi("wrtbwmon/config")),_("Configuration"), 20).leaf=true
+	entry({"admin", "status", "usage", "custom"},form("wrtbwmon/custom"),_("User file"), 30).leaf=true
 end
 
