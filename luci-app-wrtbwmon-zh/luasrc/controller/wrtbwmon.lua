822505e146bbd88d39d4ba8b6506404511e38eee
firkerword <61251923+firkerword@users.noreply.github.com>
2022-12-14 00:51:14 +0800

    Update wrtbwmon.lua

--- luci-app-wrtbwmon-zh/luasrc/controller/wrtbwmon.lua
+++ luci-app-wrtbwmon-zh/luasrc/controller/wrtbwmon.lua
@@ -1,18 +1,12 @@
-
 module("luci.controller.wrtbwmon", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/wrtbwmon") then return end
-
-	if nixio.fs.access("/var/run/wrtbwmon.pid") then
+	if not nixio.fs.access("/etc/config/wrtbwmon") then 
+                return
+        end
 	entry({"admin", "status", "usage"},alias("admin", "status", "usage", "details"), _("Traffic Status"), 60)
 	entry({"admin", "status", "usage", "details"}, template("wrtbwmon"),_("Details"), 10).leaf=true
 	entry({"admin", "status", "usage", "config"}, arcombine(cbi("wrtbwmon/config")),_("Configuration"), 20).leaf=true
 	entry({"admin", "status", "usage", "custom"}, form("wrtbwmon/custom"),_("User file"), 30).leaf=true
-	else
-	entry({"admin", "status", "usage"},alias("admin", "status", "usage", "details"), _("Traffic Status"), 60)
-	entry({"admin", "status", "usage", "details"}, template("wrtbwmon_"),_("Details"), 10).leaf=true
-	entry({"admin", "status", "usage", "config"}, arcombine(cbi("wrtbwmon/config")),_("Configuration"), 20).leaf=true
-	entry({"admin", "status", "usage", "custom"}, form("wrtbwmon/custom"),_("User file"), 30).leaf=true
-	end
 end
+
