a7ddb751c3960e0754aeab1aa991d493034ca916
firker <56651759+firker@users.noreply.github.com>
2023-02-21 23:51:01 +0800

    Create autotimeset.lua

--- luci-app-autotimeset/luasrc/controller/autotimeset.lua
+++ luci-app-autotimeset/luasrc/controller/autotimeset.lua
@@ -1,4 +1,4 @@
-module("luci.systemler.autotimeset",package.seeall)
+module("luci.controller.autotimeset",package.seeall)
 function index()
 	if not nixio.fs.access("/etc/config/autotimeset") then
 		return
